88180417c47e915fb5b7f22225b72072b9b4637e
Cal-ctrl <callum.mcneil89@gmail.com>
2022-02-12 15:52:48 +0000

    Spelling Errors corrected Retail Bible

--- client/src/components/RetailBibleHeader.jsx
+++ client/src/components/RetailBibleHeader.jsx
@@ -8,7 +8,7 @@ function RetailBibleHeader () {
 
     return <Container>
     <h1 className='menu-head'>Retail Bible Assistant</h1>
-    <p>This page shows your Stock rooms allows you to update where items are, date tracking perishables and being able to track low date stock for the entire building in one place, and you can upload your Stock inquiry, product sale and wastage for the same period adn this will show you your weekly reports. Some erres still need invetigating but the ususal issues are worked out for you! A hander forcaster for you as weel! </p>
+    <p>This page shows; your Stock rooms, allows you to update where items are, date tracking perishables and being able to track low date stock for the entire building in one place. On upload of your Stock inquiry, Product Sale and Wastage reports for the same period, this will show you miss rining wastage errors, previous week errors and misc errors. Some areas still need invetigating amd post actions need executing but the ususal issues are worked out for you! A handy forcaster for you as weel! </p>
     <br />
     </Container>
 }
